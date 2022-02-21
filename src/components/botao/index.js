9eafd74431dabf56dd71a4827d30cda23f5fa4b8
Rafael Coelho <rafaelcoe@gmail.com>
2022-02-21 17:05:48 -0300

    renderizando botao

---
+++ src/components/botao/index.js
@@ -0,0 +1,26 @@
+import './style.css';
+
+class Botao {
+	constructor() {
+		this.title = '';
+		this.callback = function () {};
+	}
+
+	setTitle(t) {
+		this.title = t;
+	}
+
+	setClick(f) {
+		this.callback = f;
+	}
+
+	render() {
+		let b = document.createElement('button');
+		b.classList.add('botao');
+		b.addEventListener('click', this.callback);
+
+		return b;
+	}
+}
+
+export default Botao;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import $ from 'jquery';
 import './css/style.css';
+import Botao from './components/botao/index.js';
+
+$(function () {
+	let botao = new Botao();
+	botao.setTitle('Testador');
+	botao.setClick(function () {
+		alert('Clicou!');
+	});
+
+	$('.area').html(botao.render());
+});
